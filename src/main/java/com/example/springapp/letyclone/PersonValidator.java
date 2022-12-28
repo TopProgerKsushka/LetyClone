10c2f7af4110b8e32aac4f2d714f821e7bbd1fa9
mat973 <48097405+mat973@users.noreply.github.com>
2022-12-28 23:50:50 +0300

    Create PersonValidator.java
    Validator for person

---
+++ src/main/java/com/example/springapp/letyclone/PersonValidator.java
@@ -0,0 +1,24 @@
+@Component
+public class PersonValidator implements Validator {
+
+    private final PersonDAO personDAO;
+
+    @Autowired
+    public PersonValidator(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return Person.class.equals(aClass);
+    }
+
+    @Override
+    public void validate(Object o, Errors errors) {
+        Person person = (Person) o;
+
+        if (personDAO.getPersonByFullName(person.getFullName()).isPresent()) {
+            errors.rejectValue("Name", "", "Человек с таким именем уже существует");
+        }
+    }
+}
